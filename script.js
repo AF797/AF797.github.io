733b5f206c7aaacffb262bdabfdee3eeadd8c836
AF797 <86837707+AF797@users.noreply.github.com>
2023-04-27 13:37:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ async function start() {
     if (image) image.remove()
     if (canvas) canvas.remove()
     image = await faceapi.bufferToImage(imageUpload.files[0])
+    image.style.width = '500px';
+    image.style.height = 'auto';
     container.append(image)
     canvas = faceapi.createCanvasFromMedia(image)
     container.append(canvas)
