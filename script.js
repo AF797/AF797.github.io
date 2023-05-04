1fa4b9dcdc3d38bf165456c4a733e184ca1912f3
AF797 <86837707+AF797@users.noreply.github.com>
2023-05-04 16:33:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -29,29 +29,31 @@ async function start() {
     const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+    document.getElementById("result").innerHTML=""; // lotto reset
     results.forEach((result, i) => {
+      //-----------------------------------------------------------------------------------------
+      var lotto = [];
+      for(var i=0;i<6;i++){
+        var num = Math.floor(Math.random() * 44) + 1;
+        for(var j in lotto){
+          while(num == lotto[j]){
+            num = Math.floor(Math.random() * 44) + 1;
+          }
+        }
+        lotto.push(num);
+      }
+      lotto.sort(function(a,b){
+        return a - b;
+      });
+      const str = '<b><font size="5em" color=red>\n얼굴분석 로또 번호</font> : <font size="5em">' + lotto + '</font></b>'
+
+      document.getElementById("result").innerHTML=str;
+      //document.body.append().innerHTML=str;
+      //-----------------------------------------------------------------------------------------
       const box = resizedDetections[i].detection.box
       const drawBox = new faceapi.draw.DrawBox(box, { label: 'face' })
       drawBox.draw(canvas)
     })
-
-    var lotto = [];
-    for(var i=0;i<6;i++){
-      var num = Math.floor(Math.random() * 44) + 1;
-      for(var j in lotto){
-        while(num == lotto[j]){
-          num = Math.floor(Math.random() * 44) + 1;
-        }
-      }
-      lotto.push(num);
-    }
-    lotto.sort(function(a,b){
-      return a - b;
-    });
-    const str = '<b><font size="5em" color=red>\n얼굴분석 로또 번호</font> : <font size="5em">' + lotto + '</font></b>'
-
-    document.getElementById("result").innerHTML=str;
-    //document.body.append().innerHTML=str;
   })
 }
 
